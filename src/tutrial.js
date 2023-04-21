a918b89b7dd843df00423fe11b26061261850054
kkito0726 <k.kito0726@gmail.com>
2023-04-21 19:42:00 +0900

    initial commit

---
+++ src/tutrial.js
@@ -0,0 +1,8 @@
+// const a: Number = 3;
+// const b: Number = 5;
+// console.log(b);
+var array1 = [1, 2, 3, 4];
+var array2 = [];
+array2 = array1;
+console.log(array2);
+console.log(__dirname);
---
+++ src/tutrial.ts
@@ -0,0 +1,9 @@
+// const a: Number = 3;
+// const b: Number = 5;
+
+// console.log(b);
+
+let array1: number[] = [1, 2, 3, 4];
+let array2: number[] = [];
+array2 = array1;
+console.log(array2);
